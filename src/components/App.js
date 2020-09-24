e3fecead6e2ae245cb9033f95917dc3699af1f80
Martin Luna <martin.luna@possible.com>
2020-09-24 15:42:09 -0300

    first commit, added components for searcbar and results, remove starter files

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get(
+      "https://api.unsplash.com/search/photos?",
+      {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID Pu1QS8uQszE3XOTYwOqbY6TVajlhU6HTXeqSMXTJ2W4",
+        },
+      }
+    );
+
+    this.setState({ images: response.data.results });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <label>Image search</label>
+          <input
+            type="text"
+            value={this.state.term}
+            className="field"
+            onChange={(e) => this.setState({ term: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
